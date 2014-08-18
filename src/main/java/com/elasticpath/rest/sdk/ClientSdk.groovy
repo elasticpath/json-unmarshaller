d9abdfdc3940a4314bbe43bf45919233a05c173f
Dominic Jones <dominic.jones@elasticpath.com>
2014-08-18 16:26:39 -0700

    Tidying

--- src/main/java/com/elasticpath/rest/sdk/ClientSdk.groovy
+++ src/main/java/com/elasticpath/rest/sdk/ClientSdk.groovy
@@ -1,12 +1,12 @@
 package com.elasticpath.rest.sdk
 
+import static javax.ws.rs.client.ClientBuilder.newClient
 import static javax.ws.rs.client.Entity.form
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE
 
-import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.core.Form
-
-import groovy.json.JsonOutput
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriBuilder
 
 import com.elasticpath.rest.sdk.model.Auth
 import com.elasticpath.rest.sdk.model.Linkable
@@ -15,40 +15,57 @@ class ClientSdk {
 
 	static void main(String[] args) {
 
-		def accessToken = auth()
+		def scope = 'mobee'
+		def serverPath = cortexUri()
+		def accessToken = auth(
+				scope,
+				serverPath.clone()
+						.path('oauth2')
+						.path('tokens')
+		)
 
-		def response = ClientBuilder.newClient()
-				.register(JacksonProvider)
-				.target('http://localhost:9080/cortex/root/mobee')
-				.request(APPLICATION_JSON_TYPE)
-				.header('Authorization', "Bearer $accessToken")
-				.get()
-
+		def href = serverPath.path('root')
+				.path(scope)
+		Response response = get(href, accessToken)
 		def linkable = response.readEntity(Linkable)
 
-		def response2 = ClientBuilder.newClient()
+		def href2 = UriBuilder.fromPath(linkable.links[0].href)
+		def response2 = get(href2, accessToken)
+		println response2.readEntity(Linkable).links.asCollection().rel
+	}
+
+	static Response get(UriBuilder href, String accessToken) {
+		newClient()
 				.register(JacksonProvider)
-				.target(linkable.links[0].href)
+				.target(href)
 				.request(APPLICATION_JSON_TYPE)
 				.header('Authorization', "Bearer $accessToken")
 				.get()
-		println JsonOutput.prettyPrint(response2.readEntity(String))
 	}
 
-	static auth() {
+	static auth(String scope, UriBuilder path) {
 		Form auth = new Form()
 				.param('grant_type', 'password')
 				.param('username', 'ben.boxer@elasticpath.com')
 				.param('password', 'password')
 				.param('role', 'REGISTERED')
-				.param('scope', 'mobee')
+				.param('scope', scope)
 
-		def response = ClientBuilder.newClient()
+		def response = newClient()
 				.register(JacksonProvider)
-				.target('http://localhost:9080/cortex/oauth2/tokens')
+				.target(path)
 				.request(APPLICATION_JSON_TYPE)
 				.post(form(auth))
 
-		response.readEntity(Auth).accessToken
+		response.readEntity(Auth)
+				.accessToken
+	}
+
+	static def cortexUri() {
+		UriBuilder.newInstance()
+				.scheme('http')
+				.host('localhost')
+				.port(9080)
+				.path('cortex')
 	}
 }
