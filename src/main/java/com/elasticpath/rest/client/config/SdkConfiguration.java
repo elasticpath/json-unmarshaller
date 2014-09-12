b3dd418bf8f98cadbac4a34a34acd54c7527b9e4
Dominic Jones <dominic.jones@elasticpath.com>
2014-09-12 15:55:19 -0700

    Uncertain if we need to export these beans at this time

--- src/main/java/com/elasticpath/rest/client/config/SdkConfiguration.java
+++ src/main/java/com/elasticpath/rest/client/config/SdkConfiguration.java
@@ -2,15 +2,11 @@ package com.elasticpath.rest.client.config;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
-import javax.ws.rs.client.Client;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.elasticpath.rest.client.DefaultCortexClient;
 
 @Configuration
 @ComponentScan("com.elasticpath.rest")
@@ -20,15 +16,5 @@ public class SdkConfiguration {
 	ObjectMapper objectMapper() {
 		return new ObjectMapper()
 				.disable(FAIL_ON_UNKNOWN_PROPERTIES);
-	}
-
-	@Bean
-	Client authClient(DefaultCortexClient cortexClient) {
-		return cortexClient.newAuthClient();
-	}
-
-	@Bean
-	Client cortexClient(DefaultCortexClient cortexClient) {
-		return cortexClient.newOAuth2RestClient();
 	}
 }
