82085624e57fd0ed332b2488f9e4570044dc4e8d
Dominic Jones <dominic.jones@elasticpath.com>
2014-09-24 14:30:08 -0700

    Re-adding injection logic to request filter

--- src/main/java/com/elasticpath/rest/client/oauth2/OAuth2RequestFilter.java
+++ src/main/java/com/elasticpath/rest/client/oauth2/OAuth2RequestFilter.java
@@ -2,15 +2,21 @@ package com.elasticpath.rest.client.oauth2;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
 
 import com.elasticpath.rest.client.oauth2.model.OAuth2Token;
 
+@Named
+@Singleton
 public class OAuth2RequestFilter implements ClientRequestFilter {
 
 	private OAuth2TokenService tokenService;
 
+	@Inject
 	public OAuth2RequestFilter(OAuth2TokenService tokenService) {
 		this.tokenService = tokenService;
 	}
