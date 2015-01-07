934caf5be997fd950ae145bf9ebe84e1de26e0b7
pmatich <Paul.Matich@elasticpath.com>
2015-01-06 16:34:35 -0800

    [PB-371] - added try statement to close input streams

--- src/main/java/com/elasticpath/rest/client/unmarshalling/annotations/CheckJsonAnnotations.java
+++ src/main/java/com/elasticpath/rest/client/unmarshalling/annotations/CheckJsonAnnotations.java
@@ -1,6 +1,5 @@
 package com.elasticpath.rest.client.unmarshalling.annotations;
 
-
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,7 +8,7 @@ import com.jayway.jsonpath.InvalidPathException;
 import com.jayway.jsonpath.internal.PathCompiler;
 
 /**
- * Tests for {@link temp}
+ * Tests for {@link CheckJsonAnnotations}
  */
 public class CheckJsonAnnotations {
 
@@ -24,7 +23,6 @@ public class CheckJsonAnnotations {
 			"@Json" + TEXT + QUOTE + REFERABLE_TEXT + QUOTE + CLOSING_BRACKET + MAKE_PRECEDING_TOKEN_OPTIONAL;
 
 	public static void main(final String[] args) throws Exception {
-		System.out.println("<><><><><><><><><>");
 		CheckJsonAnnotations annotationChecker = new CheckJsonAnnotations();
 		annotationChecker.checkJsonAnnotationsRecursivelyFromFileOrDirectoryNames(args);
 
@@ -34,26 +32,31 @@ public class CheckJsonAnnotations {
 
 	}
 
-	private void checkJsonAnnotationsInFile(File file) throws IOException, InvalidPathException {
+	//visible for testing
+	protected void checkJsonAnnotationsInFile(File file) throws IOException, InvalidPathException {
 		Pattern pattern = Pattern.compile(ANNOTATION_PATH_PATTERN);
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		for(String line; (line = br.readLine()) != null; ) {
-			line = line.replaceAll("\t","");
-			line = line.trim();
-			if (line.startsWith("@JsonProperty")||line.startsWith("@JsonPath")) {
-				Matcher m = pattern.matcher(line);
-				if (m.find()){
-					String path = m.group(1);
-					if (path.equals("")) {
-						throw new InvalidPathException("Json Annotation cannot be empty");
+
+		try(InputStream fileIs = new FileInputStream(file)) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(fileIs));
+			for (String line; (line = br.readLine()) != null; ) {
+				line = line.replaceAll("\t", "");
+				line = line.trim();
+				if (line.startsWith("@JsonProperty") || line.startsWith("@JsonPath")) {
+					Matcher m = pattern.matcher(line);
+					if (m.find()) {
+						String path = m.group(1);
+						if (path.equals("")) {
+							throw new InvalidPathException("Json Annotation cannot be empty");
+						}
+						PathCompiler.compile(path);
 					}
-					PathCompiler.compile(path);
 				}
 			}
 		}
 	}
 
-	private void checkJsonAnnotationsRecursivelyFromFileOrDirectory(File fileOrDirectory) throws IOException {
+	//visible for testing
+	protected void checkJsonAnnotationsRecursivelyFromFileOrDirectory(File fileOrDirectory) throws IOException {
 		if (fileOrDirectory.isDirectory()) {
 			File[] files = fileOrDirectory.listFiles();
 			for (File file : files) {
